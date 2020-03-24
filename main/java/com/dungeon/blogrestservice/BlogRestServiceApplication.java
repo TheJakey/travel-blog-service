91494a21c479cd674cc38ec2a38d5744850a02d5
unknown <jakub.marincic@gmail.com>
2020-03-24 21:49:14 +0100

    adding src folder and base from initializer

---
+++ main/java/com/dungeon/blogrestservice/BlogRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dungeon.blogrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogRestServiceApplication.class, args);
+	}
+
+}
