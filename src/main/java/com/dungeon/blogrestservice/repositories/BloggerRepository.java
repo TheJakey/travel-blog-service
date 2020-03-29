f9d3a065f87cb330c4f041bc88b54b8e7a4003be
Jakub Marinčič <32109375+TheJakey@users.noreply.github.com>
2020-03-29 13:03:16 +0200

    Revert "Registration"

--- src/main/java/com/dungeon/blogrestservice/repositories/BloggerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dungeon.blogrestservice.repositories;
-
-import com.dungeon.blogrestservice.models.Blogger;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BloggerRepository extends CrudRepository<Blogger, Long> {
-}
